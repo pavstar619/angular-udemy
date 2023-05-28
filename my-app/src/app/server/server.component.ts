4790ad1957590b1070e3e8e96095ad1b8d79a74b
pavelrahman619 <pavel.rahman@savethechildren.org>
2023-05-28 14:51:52 +0600

    ngClass

--- my-app/src/app/server/server.component.ts
+++ my-app/src/app/server/server.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online{
+      background-color:yellow;
+    }
+  `]
 })
 
 
@@ -11,10 +16,10 @@ export class ServerComponent {
   serverId: number = 1;
   status: string = 'offline';
 
-  getStatus(){
+  getStatus() {
     return this.status;
   }
-  getColor(){
+  getColor() {
     return this.status === 'offline' ? 'red' : 'green';
   }
 }
